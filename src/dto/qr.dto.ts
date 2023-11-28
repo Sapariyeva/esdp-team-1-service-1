bb29d9e2334154d3ef517f20e6eda8011db1709c
Alex Emel <sirsulic@gmail.com>
2023-11-28 16:37:44 +0600

    Time frame validation for qr dto #9
    valid_from must be equal or more than current time - 1 min
    valid_to must be at least 1 hour later than valid_from

--- src/dto/qr.dto.ts
+++ src/dto/qr.dto.ts
@@ -1,3 +1,4 @@
+import { IsValidFromDate, IsValidToDate } from '@/dto/validators/qr.validator';
 import { Expose } from 'class-transformer';
 import { ArrayMinSize, IsArray, IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from 'class-validator';
 
@@ -23,14 +24,15 @@ export class QrDTO {
   @Expose()
   @IsNotEmpty()
   @IsNumber()
+  @IsValidFromDate({ message: 'Valid from date must be equal or more current time minus 1 minute' })
   valid_from!: number;
 
   @Expose()
   @IsNotEmpty()
   @IsNumber()
+  @IsValidToDate('valid_from', { message: 'Valid to date must be at least 1 hour later than valid from' })
   valid_to!: number;
 }
-
 
 export class QrValidateDTO {
   @Expose()
