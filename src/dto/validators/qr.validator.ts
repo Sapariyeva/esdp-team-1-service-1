7cf657b454b9e7c1f485065741b7118f4c3f4243
OlegG <oleg.a.g.post@gmail.com>
2024-01-22 21:40:53 +0700

    redundant validation has been removed

--- src/dto/validators/qr.validator.ts
+++ src/dto/validators/qr.validator.ts
@@ -1,58 +1,26 @@
 import {
-  ValidationArguments,
   ValidationOptions,
   ValidatorConstraint,
   ValidatorConstraintInterface,
   registerDecorator,
 } from 'class-validator';
 
-@ValidatorConstraint({ async: true })
-export class IsValidFromDateConstaint implements ValidatorConstraintInterface {
-  async validate(dateFrom: number, args: ValidationArguments) { 
-    const datetime = new Date(dateFrom);
-    if (datetime instanceof Date) {
-      const now = new Date().getTime();
-      return dateFrom >= now - 60 * 1000 ? true : false;
-    } else {
-      return false;
-    }
-  }
-}
-
-export function IsValidFromDate(validationOptions?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
+export function IsValidDate(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: any) {
     registerDecorator({
       target: object.constructor,
       propertyName: propertyName,
       options: validationOptions,
       constraints: [],
-      validator: IsValidFromDateConstaint
+      validator: IsValidDateConstraint
     });
   };
 }
 
 @ValidatorConstraint({ async: true })
-export class IsValidToDateConstaint implements ValidatorConstraintInterface {
-  async validate(dateTo: number, args: ValidationArguments) {
+export class IsValidDateConstraint implements ValidatorConstraintInterface {
+  async validate(dateTo: number) {
     const datetime = new Date(dateTo);
-    if (datetime instanceof Date) {
-      const [dateFrom] = args.constraints;
-      const dateFromValue = (args.object as any)[dateFrom] as number;
-      return dateTo - dateFromValue > 3600 * 1000 ? true : false;
-    } else {
-      return false;
-    }
+    return (datetime instanceof Date) ? true : false
   }
-}
-
-export function IsValidToDate(property: string, validationOptions?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
-    registerDecorator({
-      target: object.constructor,
-      propertyName: propertyName,
-      options: validationOptions,
-      constraints: [property],
-      validator: IsValidToDateConstaint,
-    });
-  };
 }
